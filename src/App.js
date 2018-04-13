477d08944698793843f2bcba2c19a317fbd569cf
Michal Kapiczynski <michal.kapiczynski@sirenia.eu>
2018-04-14 00:13:21 +0200

    Adding dropzone component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { render } from "react-dom";
-// import "./styles.css";
 import { Organizer } from "./organizer";
 import _ from "lodash";
 import axios from "axios";
+import Dropzone from 'react-dropzone'
 
 class App extends React.Component {
     state = {
@@ -23,6 +23,19 @@ class App extends React.Component {
         this.setState({ organizers: response.data });
     }
 
+    onDrop(files) {
+        files.forEach(f => {
+            var reader = new FileReader();
+            reader.readAsText(f, "UTF-8");
+            reader.onload = function (evt) {
+                console.log(evt.target.result)
+            }
+            reader.onerror = function (evt) {
+                console.log("error reading file")
+            }
+        })
+    }
+
     render() {
         const organizers = this.state.organizers;
         const sortedOrganizers = _.sortBy(organizers, "votes").reverse();
@@ -35,8 +48,22 @@ class App extends React.Component {
             />
         ));
 
-        return <ul>{organizersComponents}</ul>;
+
+
+        return (
+            <section>
+                <div className="dropzone">
+                    <Dropzone onDrop={this.onDrop.bind(this)}>
+                        <p>Try dropping some files here, or click to select files to upload.</p>
+                    </Dropzone>
+                </div>
+                <div>
+                <ul>{organizersComponents}</ul>
+                </div>
+            </section>
+        );
     }
+
 }
 
 export default App;
