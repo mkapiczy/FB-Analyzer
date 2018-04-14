5ebc6aeb65fe4e0e04e41c209deed29fd7e3b028
Michal Kapiczynski <michal.kapiczynski@sirenia.eu>
2018-04-14 23:10:40 +0200

    Slider functionality

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+Array.range = (start, end) => Array.from({length: (end - start)}, (v, k) => k + start);
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
