1c15389d9b9137c604cdaa57e9ee5acb68936b12
Michal Kapiczynski <michal.kapiczynski@sirenia.eu>
2018-04-15 10:30:53 +0200

    Making usage instruction more detail

--- src/InfoBoard.js
+++ src/InfoBoard.js
@@ -1,22 +1,26 @@
 import React from "react";
 import "./InfoBoard.css";
+
 export class InfoBoard extends React.Component {
 
     render() {
         return (
-            
+
             <ol classNmae="info-list">
                 <div className="header">How to Use FBDA</div>
                 <li>Download your data from <a href="wwww.facebook.com">facebook</a>:
-                once logged in, go to settings and click "Download a copy of your facebook data". 
-                You'll retrieve your data via email (may take quite a while, depending on the
-                size of your data).
+                    once logged in, go to settings and click "Download a copy of your facebook data".
+                    You'll retrieve your data via email (may take quite a while, depending on the
+                    size of your data).
                 </li>
-                <li>Once you have received the zip file of your data via email, unzip it, and remove every file that is not an html.
+                <li>Once you have received the zip file with your data via email, unzip it, and remove every file that
+                    does not have an .htm/.html extension (videos, photos, audio, gifs, stickers etc.). Remember to remove
+                    them also from subdirectories!
                 </li>
-                <li>Zip the remaining files, and now you are ready to upload for data analysis!</li>
-                <li>Upload the data by either dragging and dropping your zipfile or by selecting from the window.</li>
-                <li>View your analysis results</li>
+                <li>Zip the remaining files, and now you are ready to upload the created zip archive for the data analysis!</li>
+                <li>Upload the data by either dragging and dropping your zipfile into dropzone area below or by clicking on it and selecting the file from the window.</li>
+                <li>View your analysis results.</li>
+                <li>Filter your results using a slider and a search box!</li>
             </ol>
         )
     }
